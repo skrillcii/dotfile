8278e734f8cdf200f3e12d9976d32bcfe913490f
skrillcii <skrillcii>
2019-04-16 13:47:04 +0900

    update zsh-autosuggestions install script

--- sinstall.sh
+++ sinstall.sh
@@ -112,6 +112,10 @@ oh_my_zsh_install()
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 }
 
+zsh_autosuggestions()
+{
+    git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
+}
 
 ranger_install()
 {
