b0235f66a8b774af0c525c10133e1c874cd6e4da
skrillcii <skrillcii@github.com>
2020-09-07 11:44:00 +0900

    updated i3exit.sh

--- i3exit.sh
+++ i3exit.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 # To create symbolic link in /usr/local/bin, run the command below
-# sudo ln -s /full/path/to/your/i3wm.sh /usr/local/bin/name_of_new_command
+# sudo ln -s ~/dotfile/i3exit.sh /usr/local/bin/i3exit
 
 # After creating the symbolic link, make this file executable by following command:
-# chmod +x /full/path/to/your/i3wm.sh
+# chmod +x /full/path/to/your/i3exit.sh
 
 lock() {
     i3lock
